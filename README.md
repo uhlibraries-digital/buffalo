d72c8ec43ba1d6b7d46fd7f827d3ddf4dc4be5b0
Andrew Weidner <metaweidner@gmail.com>
2018-08-20 09:18:02 -0500

    add migration report

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 Bayou City DAMS digital collections migration utilities
 
 ---
+
+## migration_report.rb
+
+This script creates reports for the BCDAMS-MAP Migration site: https://vocab.lib.uh.edu/bcdams-map/migration
+
+Usage: In a command prompt, enter a CDM collection alias, crosswalk file name from the 'dmwg' directory, and your name as shown in your O: drive config file.
+Example: `ruby migration_report.rb p15195coll38 p15195coll38_crosswalk andy`
+
+You must have a valid config file in the following folder: `O:\Metadata and Digitization Services\Metadata\Migration\config`
+Example Config: `andy.yml`
+```yaml
+dmwg: 'M:\Software\dmwg'
+map: 'M:\Software\bcdams-map'
+items:
+```
+
+`dmwg:` points to the location of your local [dmwg](https://github.com/uhlibraries-digital/dmwg) repository clone.
+`map:` points to the location of your local [bcdams-map](https://github.com/uhlibraries-digital/bcdams-map) repository clone.
+`items:` identifies the CDM numbers for a collection subset in the following format: `[363,350,358]`. This must be left blank (shown in the Example Config) if the report should include all items in a collection.
+
+After running this script, push the changes to the BCDAMS-MAP repository. Results will be immediately available here: https://uhlibraries-digital.github.io/bcdams-map/migration/
---
+++ migration_report.rb
@@ -0,0 +1,43 @@
+require 'hunting'
+require './lib/buffalo.rb'
+require './lib/buffalo/cdm.rb'
+require './lib/buffalo/map.rb'
+require './lib/buffalo/crosswalk.rb'
+require './lib/buffalo/element.rb'
+require './lib/buffalo/report.rb'
+require './lib/buffalo/aspace.rb'
+require 'csv'
+require 'yaml'
+require 'json'
+require 'open-uri'
+require 'linkeddata'
+require 'uri'
+require 'active_support'
+require 'active_support/core_ext'
+require 'httparty'
+require 'securerandom'
+
+COLLECTION = ARGV[0]
+CROSSWALK = ARGV[1]
+NAME = ARGV[2]
+
+if COLLECTION == nil or CROSSWALK == nil or NAME == nil
+  puts "\nPlease enter a CDM collection alias, crosswalk file name from the 'dmwg' directory, and your name.\n\n=== Example ===\nruby migration_report.rb p15195coll38 p15195coll38_crosswalk andy\n"
+  exit
+else
+  Hunting.configure_with("O:/Metadata and Digitization Services/Metadata/Migration/config/uhdl.yml")
+  Buffalo.configure_with("O:/Metadata and Digitization Services/Metadata/Migration/config/#{NAME}.yml")
+  DMWG = Buffalo.config[:dmwg]
+  MAP = Buffalo.config[:map]
+  ITEMS = Buffalo.config[:items]
+  puts ''
+  puts 'Loading MAP...'
+  bcdams = Map.new(JSON.parse(open('https://vocab.lib.uh.edu/bcdams-map/api/elements.json').read))
+  puts 'Loading Crosswalk...'
+  elements = Crosswalk.elements("#{DMWG}/#{COLLECTION}/#{CROSSWALK}.yml", bcdams)
+  uhdl = Repository.scout([COLLECTION])
+  uhdl.collections.each do |c_alias, collection|
+    filename = Report.migration(collection, elements, { path: "#{MAP}/migration", cdm_domain: "digital.lib.uh.edu", items: ITEMS })
+    Buffalo.append("#{MAP}/_data/migration.yml", "\n- label: #{@filename}\n  file: #{filename}\n")
+  end
+end
