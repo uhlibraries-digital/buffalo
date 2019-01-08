adf874560f86487d84c66003fa973aee85a986d4
Andrew Weidner <metaweidner@gmail.com>
2019-01-08 13:43:15 -0600

    add artificial items and standard projects

--- lib/buffalo/carp.rb
+++ lib/buffalo/carp.rb
@@ -1,16 +1,21 @@
 module Carp
 
   def self.archival(collection, elements, project)
-    aspace = ASpace.new(project[:aspace]['endpoint'], project[:aspace]['username'], project[:aspace]['password'])
+    aspace = ASpace.new(
+      project[:aspace]['endpoint'],
+      project[:aspace]['username'],
+      project[:aspace]['password']
+    )
     resource = aspace.get_object(project[:collection_uri])
-    report = {}
-    report[:type] = 'findingaid'
-    report[:resource] = resource['uri']
-    report[:collectionTitle] = resource['title']
-    report[:aic] = ''
+    carp_file = {}
+    carp_file[:type] = 'findingaid'
+    carp_file[:resource] = resource['uri']
+    carp_file[:collectionTitle] = resource['title']
+    carp_file[:aic] = ''
     objects = []
     collection.objects.each do |pointer, object|
       aspace_object = aspace.get_object(object.metadata['ArchivesSpace URI'])
+      level = aspace_object['level']
       begin
         sub_container = aspace_object['instances'][0]['sub_container']
       rescue
@@ -25,53 +30,100 @@ module Carp
         puts aspace_object
         next
       end
+
       archival_object = {}
       archival_object[:uuid] = SecureRandom.uuid
-      level = aspace_object['level']
-      dates = []
-      if level == 'item'
-        if aspace_object['dates'].nil?
-          continue
-        else
-          aspace_object['dates'].each {|date| dates << date['expression']}
-        end
-        archival_object[:title] = aspace_object['title']
-        archival_object[:dates] = dates
-        containers = []
-        containers << {:top_container => {:ref => container['uri']},
-                       :type_1 => container['type'],
-                       :indicator_1 => container['indicator'],
-                       :type_2 => sub_container['type_2'],
-                       :indicator_2 => sub_container['indicator_2'],
-                       :type_3 => sub_container['type_3'],
-                       :indicator_3 => sub_container['indicator_3']}
-        archival_object[:containers] = containers
-        archival_object[:uri] = aspace_object['uri']
-      else
-        # artificial items attached to parent
-      end
       archival_object[:files] = []
-      archival_object[:productionNotes] = ''
-      archival_object[:do_ark] = ''
-      archival_object_metadata = {}
-      metadata = CDM.metadata(object, elements)
-      metadata.each do |k,v|
-        elements.each do |element|
-          if element.name == k
-            @namespace = element.namespace
-          end
-        end
-        archival_object_metadata["#{@namespace}.#{k}"] = CDM.values_string(v,';')
+      archival_object[:productionNotes] = "CDM: #{pointer}"
+
+      dates = []
+      if aspace_object['dates'].nil?
+        continue
+      else
+        aspace_object['dates'].each {|date| dates << date['expression']}
       end
-      archival_object[:metadata] = archival_object_metadata
+      archival_object[:dates] = dates
+
+      containers = []
+      if level == 'item'
+        archival_object[:title] = aspace_object['title']
+        archival_object[:uri] = aspace_object['uri']
+        containers << {
+          :top_container => {:ref => container['uri']},
+          :type_1 => container['type'],
+          :indicator_1 => container['indicator'],
+          :type_2 => sub_container['type_2'],
+          :indicator_2 => sub_container['indicator_2'],
+          :type_3 => sub_container['type_3'],
+          :indicator_3 => sub_container['indicator_3']
+        }
+      else
+        archival_object[:artificial] = true
+        archival_object[:title] = object.metadata['Title']
+        archival_object[:parent_uri] = object.metadata['ArchivesSpace URI']
+        archival_object[:level] = 'item'
+        containers << {
+          :type_1 => container['type'],
+          :indicator_1 => container['indicator'],
+          :type_2 => sub_container['type_2'],
+          :indicator_2 => sub_container['indicator_2'],
+          :type_3 => 'Item',
+          :indicator_3 => pointer
+        }
+      end
+      archival_object[:containers] = containers
+
+      cdm_metadata = CDM.metadata(object, elements)
+      archival_object[:metadata] = Carp.format_metadata(cdm_metadata, elements)
       objects << archival_object
     end
-    report[:objects] = objects
-    report
+    carp_file[:objects] = objects
+    carp_file
   end
 
   def self.standard(collection, elements, project)
-    #  standard project report
+    carp_file = {}
+    carp_file[:type] = 'standard'
+    carp_file[:resource] = {
+      :title => project[:collection_uri],
+      :sip_ark => '',
+      :collectionArkUrl => project[:collection_uri],
+      :vocabTitle => '',
+      :collectionArk => project[:collection_uri].sub('https://id.lib.uh.edu/', ''),
+      :aic => ''
+    }
+    carp_file[:collectionTitle] = ''
+    carp_file[:collectionArkUrl] = project[:collection_uri]
+    carp_file[:aic] = ''
+    objects = []
+    collection.objects.each do |pointer, object|
+      standard_object = {}
+      standard_object[:uuid] = SecureRandom.uuid
+      standard_object[:title] = object.metadata['Title']
+      standard_object[:dates] = []
+      standard_object[:containers] = [{:type_1 => 'Item',:indicator_1 => pointer}]
+      standard_object[:level] = 'item'
+      standard_object[:productionNotes] = ''
+      standard_object[:files] = []
+      cdm_metadata = CDM.metadata(object, elements)
+      standard_object[:metadata] = Carp.format_metadata(cdm_metadata, elements)
+      objects << standard_object
+    end
+    carp_file[:objects] = objects
+    carp_file
+  end
+
+  def self.format_metadata(cdm_metadata, elements)
+    object_metadata = {}
+    cdm_metadata.each do |label,value|
+      elements.each do |element|
+        if element.name == label
+          @ns = element.namespace
+        end
+      end
+      object_metadata["#{@ns}.#{label}"] = CDM.values_string(value,';')
+    end
+    object_metadata
   end
 
 end
