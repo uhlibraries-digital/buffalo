2b362ca6f9e3fa5b913b16e0f0af8c046b551d81
Andrew Weidner <metaweidner@gmail.com>
2019-01-10 14:23:43 -0600

    clean up code and dependencies

--- migration_report.rb
+++ migration_report.rb
@@ -1,30 +1,27 @@
 require 'hunting'
 require './lib/buffalo'
 require './lib/buffalo/cdm'
-require './lib/buffalo/map'
 require './lib/buffalo/crosswalk'
 require './lib/buffalo/element'
+require './lib/buffalo/map'
 require './lib/buffalo/report'
-require './lib/buffalo/aspace'
-require 'csv'
-require 'yaml'
-require 'json'
-require 'open-uri'
-require 'linkeddata'
-require 'uri'
 require 'active_support'
 require 'active_support/core_ext'
-require 'httparty'
-require 'securerandom'
+require 'builder'
 require 'fileutils'
+require 'json'
+require 'open-uri'
 require 'pathname'
+require 'uri'
+require 'yaml'
 
 COLLECTION = ARGV[0]
 CROSSWALK = ARGV[1]
 NAME = ARGV[2]
 
 if COLLECTION == nil or CROSSWALK == nil or NAME == nil
-  puts "\nPlease enter a CDM collection alias, crosswalk file name from the 'dmwg' directory, and your name.\n\n=== Example ===\nruby migration_report.rb p15195coll38 p15195coll38_crosswalk andy\n"
+  puts "\nPlease enter a CDM collection alias, crosswalk file name from the 'dmwg' directory, and your name."\
+       "\n\n=== Example ===\nruby migration_report.rb p15195coll38 p15195coll38_crosswalk andy\n"
   exit
 else
   Hunting.configure_with("O:/Metadata and Digitization Services/Metadata/Migration/config/uhdl.yml")
@@ -39,7 +36,18 @@ else
   elements = Crosswalk.elements("#{DMWG}/#{COLLECTION}/#{CROSSWALK}.yml", bcdams)
   uhdl = Repository.scout([COLLECTION])
   uhdl.collections.each do |c_alias, collection|
-    filename = Report.migration_report(collection, elements, { path: "#{MAP}/migration", cdm_domain: "digital.lib.uh.edu", items: ITEMS })
-    Buffalo.append("#{MAP}/_data/migration.yml", "\n- label: \'#{filename}\'\n  file: \'#{filename}\'\n")
+    filename = Report.migration_report(
+      collection,
+      elements,
+      { path: "#{MAP}/migration",
+        cdm_domain: "digital.lib.uh.edu",
+        items: ITEMS
+      }
+    )
+    Buffalo.append(
+      "#{MAP}/_data/migration.yml",
+      "\n- label: \'#{filename}\'\n  file: \'#{filename}\'\n"
+    )
   end
+  puts "DONE"
 end
